f68ea60338a6fec20fbe57bcfe2b22834d3cff5e
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-06 16:03:23 -0400

    pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//random number gets generated (between 19-120)
+
+//four crystals get assigned random numbers/scores (between 1-12)
+
+//as the user clicks on each crystal a number gets added to score
+
+//if the user clicks on consequent crystals a number gets added to score (cumulative)
+
+//when the score goes past the random number then the user loses
+
+//when the score is a match to the number generated then the user wins
+
+//when the user wins the wins go up
+
+//when the user loses then the wins go up
+
+//whenever the player wins or loses then the game resets
