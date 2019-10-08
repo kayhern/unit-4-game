fae4effece28ed230a9cfabda1c4a8abbcbf162b
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-08 16:00:52 -0400

    jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
 //random number gets generated (between 19-120) resource: https://www.w3schools.com/jsref/jsref_random.asp
+//function to generate number:
 function randomNumber () {
     let number = Math.floor((Math.random() * 101) + 19);
     console.log(number + " random number");
     return number;
 }
 randomNumber();
+//getting the random number to show on the screen:
+
 //four crystals get assigned random numbers/scores (between 1-12)
 
 //as the user clicks on each crystal a number gets added to score
