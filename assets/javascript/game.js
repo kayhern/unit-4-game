321dc0ef3a6a8076b305479cbcb2bd7e3b2db160
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-10-12 09:32:21 -0400

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,47 @@
 //function to generate 'random' number:
-$(document).ready(function(){  
+$(document).ready(function(){
+let currentScore = 0;
+let numberb = 0;
+let numberg = 0;
+let numberr = 0;
+let numbery = 0;
+
+// Declare variables and set initial value here
+
 //random number gets generated (between 19-120) resource: https://www.w3schools.com/jsref/jsref_random.asp 
 //article explaining it's not really random: https://hackernoon.com/how-does-javascripts-math-random-generate-random-numbers-ef0de6a20131
-    function randomNumber () {
-        let number = Math.floor((Math.random() * 101) + 19);
-        console.log(number + " random number");
-        return number;  
+function randomNumber () {
+    // Remove all `let` keywords from functions
+    let number = Math.floor((Math.random() * 101) + 19);
+    console.log(number + " random number");
+    return number;  
     }
     //getting the random number to show on the screen (the number the player has to match to win the game) resource: https://api.jquery.com/html/
-    $("div.randominteger").text(randomNumber());
+$("div.randominteger").text(randomNumber());
 
 function blueValue () {
-    let numberb = Math.floor((Math.random()* 12) + 1);
+    numberb = Math.floor((Math.random()* 12) + 1);
     console.log(numberb + " blue value");
     return numberb;
 }
-let numberb= blueValue();
+blueValue();
 
 function greenValue () {
-    let numberg = Math.floor((Math.random()* 12) + 1);
+    numberg = Math.floor((Math.random()* 12) + 1);
     console.log(numberg + " green value");
     return numberg;
 }
 greenValue();
 
 function redValue () {
-    let numberr = Math.floor((Math.random()* 12) + 1);
+    numberr = Math.floor((Math.random()* 12) + 1);
     console.log(numberr + " red value");
     return numberr;
 }
 redValue();
 
 function yellowValue () {
-    let numbery = Math.floor((Math.random()* 12) + 1);
+    numbery = Math.floor((Math.random()* 12) + 1);
     console.log(numbery + " yellow value");
     return numbery;
 }
@@ -40,14 +49,34 @@ yellowValue();
 
 //need on click function for each jewel
 //need to add total score value with each jewel click
-$("img.one").click(function blueValue (){
-    let numberb = Math.floor((Math.random()* 12) + 1);
+$(".one").click(function (){
+    // let numberb = Math.floor((Math.random()* 12) + 1);
+    currentScore += numberb;
+    console.log('number b', numberb);
+    console.log('current score', currentScore);
+    $("div.totalscoreinteger").html(currentScore + numberb);
+    // Put return at end of functions
+    // return is what ends the function
     return numberb;
-    
-});
-// $("div.totalscoreinteger").text(numberb);
 });
 
+$(".two").click(function (){
+    // let numberb = Math.floor((Math.random()* 12) + 1);
+    currentScore += numberg;
+    console.log('number b', numberg);
+    console.log('current score', currentScore);
+    $("div.totalscoreinteger").html(currentScore + numberg);
+    // Put return at end of functions
+    // return is what ends the function
+    return numberg;
+});
+
+});
+
+// $("#btn-choice").click(function(event) {
+//     userChoice = this.id;
+//     test();
+// });
 
 //if the user clicks on consequent crystals a number gets added to score (cumulative)
 
